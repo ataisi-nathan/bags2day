4bd9f58c676ac5374057a6802d63201b6de3c5d3
Ataisi Nathan <ataisinathan@gmail.com>
2021-01-11 23:50:15 +0100

    Added the js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchBar = document.getElementById('searchIcon');
+
+
+
+function seek(){
+document.querySelector('ul').innerHTML='<input type="text" name="searchBar" id="search" placeholder="Search">';
+}
