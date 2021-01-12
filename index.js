d1f0d79a80484117b7f3a36e932e23a434d9091a
Ataisi Nathan <ataisinathan@gmail.com>
2021-01-12 11:29:38 +0100

    Updated the search bar

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const searchBar = document.getElementById('searchIcon');
 
 
 function seek(){
-    document.querySelector('ul').innerHTML='<input type="text" name="searchBar" id="search" placeholder="Search">';
+    document.querySelector('nav').innerHTML='<input type="text" name="searchBar" id="search" placeholder="Search"><img onclick="seeker()" id="searchIcon2" src="Assets/Images/search-icon.svg" alt="">';
+    console.log('Done1')
+}
+function seeker(){
+    document.querySelector('nav').innerHTML='<ul><li><a href="#">home</a></li><li><a href="#">contact</a></li><li><a href="#">shop</a></li></ul><img onclick="seek()" id="searchIcon" src="Assets/Images/search-icon.svg" alt="">';
+    console.log('Done2');
 }
