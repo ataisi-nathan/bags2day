640451b85df5b99470c8d71e77398a3ab20b0787
Ataisi Nathan <ataisinathan@gmail.com>
2021-01-12 10:57:52 +0100

    Updated

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const searchBar = document.getElementById('searchIcon');
 
 
 function seek(){
-document.querySelector('ul').innerHTML='<input type="text" name="searchBar" id="search" placeholder="Search">';
+    document.querySelector('ul').innerHTML='<input type="text" name="searchBar" id="search" placeholder="Search">';
 }
