183312f2b66d3a3bf5025cf5effbab30672d95b9
Ataisi Nathan <ataisinathan@gmail.com>
2021-01-13 10:56:30 +0100

    Updated the email verification bar and the search bar as well

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function seeker(){
     document.querySelector('nav').innerHTML='<ul><li><a href="#">home</a></li><li><a href="#">contact</a></li><li><a href="#">shop</a></li></ul><img onclick="seek()" id="searchIcon" src="Assets/Images/search-icon.svg" alt="">';
     console.log('Done2');
 }
+function ValidateEmail(){
+    let emailAdd = document.getElementById('email');
+    let here = document.querySelector('.warn');
+    let mailValue = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (emailAdd.value.match(mailValue)){
+        //document.write('Challenge completed!!!')
+    }
+    else{
+        here.classList.toggle('error');
+        here.classList.toggle('hide')
+        return (false);
+    }
+}
